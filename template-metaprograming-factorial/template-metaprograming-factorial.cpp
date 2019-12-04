11ea91e4b5368e338b72dc82ba209a126ae5b21c
rastosl <rsgaleg@gmail.com>
2019-12-05 00:05:53 +0100

    Add project template-metaprogramming-factorial

---
+++ template-metaprograming-factorial/template-metaprograming-factorial.cpp
@@ -0,0 +1,25 @@
+﻿// template-metaprograming-factorial.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+
+
+template <int n>
+class Fact
+{
+public:
+    static const int val = Fact<n - 1>::val * n;
+};
+
+template<>
+class Fact<0> { public: static const int val = 1; };
+
+int main()
+{
+    std::cout << "fact 4 = " << Fact<4>::val << std::endl;
+    std::cout << "fact 5 = " << Fact<5>::val << std::endl;
+    std::cout << "fact 6 = " << Fact<6>::val << std::endl;
+    std::cout << "fact 7 = " << Fact<7>::val << std::endl;
+
+    return 0;
+}
