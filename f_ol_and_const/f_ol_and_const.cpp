5f223cb411802398328d3b22f39833a1a74edbba
rastosl <rsgaleg@gmail.com>
2019-12-10 23:22:56 +0100

    Add f_ol_and_const project.
    Function overload and constness.

---
+++ f_ol_and_const/f_ol_and_const.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream> 
+
+
+using namespace std;
+
+
+int gvalue = 10;
+
+int& getValue()
+{
+    return gvalue;
+}
+
+void fun(int& i);
+
+void fun(const int& i)
+{
+    cout << "fun(const string &) called " << std::endl;
+    auto nclv = i;
+    fun(nclv);
+}
+
+void fun(int& i)
+{
+    cout << "fun(string &) called " << std::endl;
+    //const string c_s = "xz";
+    //fun(c_s);
+}
+
+int main()
+{
+    //string s = "a";
+    const auto& cr = getValue();
+    fun(cr);
+    return 0;
+}
